e691882c24c6425269ad1933b8c96f2550e00fd4
Andreas Hartung <ahartung@outlook.com>
2020-04-14 09:54:51 +0200

    running simulator initialization

--- src/runtime/riscvSimulator.ts
+++ src/runtime/riscvSimulator.ts
@@ -17,13 +17,36 @@ htmlClasses.forEach(htmlClass => {
 	global[htmlClass.name] = htmlClass
 });
 
-global["document"] = document
-global["load_update_message"] = (msg) => console.log(msg)
-global["window"] = fakeDOM.window
 global["LocalStorageManager"] = LocalStorageManager
 if (typeof localStorage === "undefined" || localStorage === null) {
 	const LocalStorage = require('node-localstorage').LocalStorage;
 	global["localStorage"] = new LocalStorage('./scratch');
+}
+const load_update_message = (msg) => console.log(msg)
+const load_done = function () {
+    load_update_message("Done!");
+    window.document.body.classList.add("loaded");
+    window.onerror = null;
+};
+global["document"] = document
+global["load_update_message"] = load_update_message
+global["load_done"] = load_done
+global["window"] = fakeDOM.window
+window["driver_load_done"] = function () {
+    /* Check if packages are all loaded */
+    const h  = function(){
+        if (driver.driver_complete_loading) {
+            load_done();
+            return
+        }
+        setTimeout(h, 10);
+    };
+    setTimeout(h, 10);
+};
+global["codeMirror"] = {
+	refresh: () => {},
+	setValue: (txt) => {},
+	save: () => {},
 }
 
 const venus = require("./venus/venus")
@@ -32,6 +55,10 @@ const venus_main = venus;
 const driver = venus_main.venus.Driver;
 venus.api = venus_main.venus.api.API;
 const simulatorAPI = venus_main.venus.api.venusbackend.simulator.Simulator;
+
+Object.assign(global, {
+	driver
+})
 
 export = {
 	venus_main,
