b0a87a1c1878de372207d6d4c2b5a5b9a592b688
Andreas Hartung <ahartung@outlook.com>
2020-08-25 12:45:06 +0200

    removed register- and cache-view

--- src/memoryui/memoryUI.js
+++ src/memoryui/memoryUI.js
@@ -1,3 +1,16 @@
+/**
+ * The view is a composite of a register-, memory-, cache-view.
+ * Only memory-view is implemented, yet.
+ * Hence there is no need for the other views.
+ * They might be useful for future development, though.
+ */
+(function removeLegacyHTMLElements() {
+	document.getElementById('register-tab-view').remove()
+	document.getElementById('cache-tab-view').remove()
+	document.getElementById('navigation-bar').remove()
+})()
+
+
 // Handle the message inside the webview
 window.addEventListener('message', event => {
 
