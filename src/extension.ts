74cd574767ce863f4d5b8ef5ebe35831f6989f1b
m1n1 <weinstab@hm.edu>
2020-08-26 11:58:15 +0200

    added manual activation of extension

--- src/extension.ts
+++ src/extension.ts
@@ -46,6 +46,13 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.executeCommand('workbench.action.openSettings', 'riscv-venus');
 	}));
 
+	context.subscriptions.push(vscode.commands.registerCommand('riscv-venus.activate', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('riscv-venus extension activated');
+	  }));
+
 	// register a configuration provider for 'venus' debug type
 	const venusProvider = new VenusConfigurationProvider();
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('venus', venusProvider));
