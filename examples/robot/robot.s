15c45682f8d1696da1b3c121593b3dea2e1a53e8
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2020-08-31 09:41:30 +0200

    Add examples for LED Matrix and Robot
    Also remove the other example.

---
+++ examples/robot/robot.s
@@ -0,0 +1,35 @@
+li a0, 0x110
+li a1, 0
+li a2, 0x0
+ecall
+li a1, 1
+ecall
+li a1, 2
+li a2, 0x0ff0
+ecall
+li a1, 3
+li a2, 0x1ff8
+ecall
+li a1, 4
+li a2, 0x2004
+ecall
+li a1, 5
+li a2, 0x0
+ecall
+li a1, 6
+li a2, 0x0810
+ecall
+li a1, 7
+li a2, 0x1c38
+ecall
+li a1, 8
+li a2, 0x1c38
+ecall
+li a1, 9
+li a2, 0x1c38
+ecall
+li a1, 10
+li a2, 0x0810
+ecall
+li x0, 0
+li x0, 0
